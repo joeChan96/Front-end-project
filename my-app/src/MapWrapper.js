08e74702eadc18e4c8d2b89560609e3eee6e71fc
joeChan96 <yanki8366@outlook.com>
2022-06-09 17:02:22 +0800

    UI modified

---
+++ my-app/src/MapWrapper.js
@@ -0,0 +1,31 @@
+import React, { useState, useRef } from "react";
+import {
+  interaction,
+  layer,
+  custom,
+  control, //name spaces
+  Interactions,
+  Overlays,
+  Controls, //group
+  Map,
+  Layers,
+  Overlay,
+  Util,
+  //objects
+} from "react-openlayers";
+import "./MapWrapper.css";
+
+export default function MapWrapper() {
+  return (
+    <main className="main-grid">
+      <div className="search">Search Heritage</div>
+      <div className="map">
+        <Map view={{ center: [0, 0], zoom: 1 }}>
+          <Layers>
+            <layer.Tile />
+          </Layers>
+        </Map>
+      </div>
+    </main>
+  );
+}
