f2990df7b0c1cd712203b4fa774623ebce3e5c92
joeChan96 <yanki8366@outlook.com>
2022-06-11 18:57:42 +0800

    Modified on 11/6

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,27 @@
 import "./App.css";
-import MapWrapper from "./MapWrapper";
-import Navpart from "./Navigation";
-import Button from "@mui/material/Button";
+import Header from "./header";
+
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { green } from "@mui/material/colors";
 
 function App() {
   return (
-    <div className="App">
-      <Navpart />
-      <MapWrapper />
-      <Button variant="contained">Hello World</Button>
+    <div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container
+          maxWidth="xl"
+          sx={{
+            bgcolor: "#f0f0f0",
+            height: "120vh",
+          }}
+        >
+          <Header />
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
---
+++ my-app/src/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+import "./HKYEAH_logo.png";
+
+export default function Header() {
+  return (
+    <nav className="header">
+      <img src="" />
+    </nav>
+  );
+}
